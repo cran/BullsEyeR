f5aa6b3c64b5bcb946f3a7f04ffe8a815ad22d9c
Krishna Harsha <khkrishnaharsha123@gmail.com>
2017-12-21 10:15:41 +0000

    version 0.2.0

--- R/BullsEyeR.R
+++ R/BullsEyeR.R
@@ -10,8 +10,6 @@
 #' Creation of Document Term Matrix -repeats first step, now including the custom stop words as well, removes empty documents if any and returns a Document term matrix. This DTM is used for finding optimal number of topics for LDA modelling  using 'FindTopicsNumber' from 'ldatuning' package
 #' @section BullsEye():
 #' Topic Modelling- Performs preprocessing along with removal of custom stop words,Uses topic number selected using 'ldatuning' and builds unigram topic model with/without stemming. Returns,
-#' @section Keywords:
-#' A data frame with keywords/dominant words of each document along with the topic number assigned by LDA model
 #' @section EmptyRows:
 #' A list of zero length documents after preprocessing
 #' @section Topics:
@@ -273,8 +271,7 @@ createDTM<-function(ds,spvar=0.99,myStopWords=NULL,stemvar=0){
 #'@param tno a number of topics to be used to model text using LDA approach which defaults to 20
 #'@param seedno seed which defaults to 12345
 #'@param stemvar a variable indicating stemming to be performed or not which defaults to '0' meaning no stemming
-#'@param stemComp type of stem completion like type = c("prevalent", "first", "longest","none", "random", "shortest")
-#'@return A dataframe with dominat words,index of empty rows  and topic terms.
+#'@return A dataframe with index of empty rows  and topic terms.
 #'@importFrom stats median
 #'@importFrom utils head tail
 #'@import tm
@@ -282,16 +279,16 @@ createDTM<-function(ds,spvar=0.99,myStopWords=NULL,stemvar=0){
 #'@import topicmodels
 #'@importFrom Matrix sparseMatrix as.matrix
 #'@import slam
-#'@export
 #'@seealso \code{\link[ldatuning]{FindTopicsNumber}}
+#'@export
 #'@examples \dontrun{
 #'# Run it and see for yourself
 #'}
 #'data.tmp<-read.csv(system.file("ext", "testdata.csv", package="BullsEyeR"))
 #'ds<-as.character(data.tmp$Story[1:2])
 #'stopwords<-c("sallin","hannah","company","number","started","unlike")
-#'BullsEye(ds=ds,spvar=0.99,myStopWords=stopwords,tno=20,seedno=12345,stemvar=0,stemComp="prevalent")
-BullsEye<-function(ds,spvar=0.99,myStopWords=NULL,tno=20,seedno=12345,stemvar=0,stemComp="prevalent"){
+#'BullsEye(ds=ds,spvar=0.99,myStopWords=stopwords,tno=20,seedno=12345,stemvar=0)
+BullsEye<-function(ds,spvar=0.99,myStopWords=NULL,tno=20,seedno=12345,stemvar=0){
 
 
   #load packages
@@ -414,122 +411,18 @@ BullsEye<-function(ds,spvar=0.99,myStopWords=NULL,tno=20,seedno=12345,stemvar=0,
   #head(topics(myModel))
   #save(myModel, file = "my_model1.rda")
 
-  ################################# Extraction of terms  #################################################
-
-  #corpus levels#not useful
-  #wordassignments_df <- data.frame (topics = myModel@wordassignments [,]$v,terms = myModel@terms [ myModel@wordassignments [ ,]$j])
-
-  #Word assignment matrix
-  simple_triplet_matrix_sparse<-myModel@wordassignments
-
-  simple_triplet_matrix_sparse <-  sparseMatrix(i=simple_triplet_matrix_sparse$i, j=simple_triplet_matrix_sparse$j, x=simple_triplet_matrix_sparse$v,
-                                                dims=c(simple_triplet_matrix_sparse$nrow, simple_triplet_matrix_sparse$ncol))
-
-
-  WA<-as.data.frame(as.matrix(simple_triplet_matrix_sparse))
-  ####
-  #tempp<-which(WA[1,]!=0)
-  #testt<-WA[1,tempp]
-  #wtd<-colnames(corp.tdm)[tempp]
-  ######
-  #put doc.term matrix currently being used
-
-  corp.tdm.sp.t.tdif<-corp.tdm.sp.t.tdif
+    corp.tdm.sp.t.tdif<-corp.tdm.sp.t.tdif
   #top topic in all documents
   t<-as.vector(topics(myModel,1))
-  ########## Extraction of top terms in top topic of each document
-  tempp2<-list()
-  wtd2<-list()
-  term1<-list()
-  logic.term<-list()
-  ordered.index<-list()
-  rep.terms<-list()
-  #terms in each topic
-  terms.df<-as.data.frame(terms(myModel,20000000))
-  #
-  representativeWords<-data.frame()
-
-  for(i in 1:dim(corp.tdm.sp.t.tdif)[1]) {
-    tempp2[[i]]<-which(WA[i,]==t[i])
-    wtd2[[i]]<-colnames(corp.tdm.sp.t.tdif)[tempp2[[i]]]
-
-
-    term1[[i]]<-as.character(terms.df[,t[i]])
-    logic.term[[i]]<-term1[[i]]%in%wtd2[[i]]
-    ordered.index[[i]]<-which(logic.term[[i]]==TRUE)
-    ordered.index[[i]]<-head(ordered.index[[i]])
-    rep.terms[[i]]<-as.character(terms.df[,t[i]][ordered.index[[i]]])
-
-  }
-
-
-  n.obs <- sapply(rep.terms, length)
-  seq.max <- seq_len(max(n.obs))
-  mat <- t(sapply(rep.terms, "[", i = seq.max))
-  mat<-t(mat)
-  #fix(mat)
-  if(stemvar==1){
-    ######################################## stem completion ################################
-    #type = c("prevalent", "first", "longest","none", "random", "shortest")
-
-    sc<-stemCompletion(mat,corp.stemComp, type = c(stemComp))
-
-    #sc1<-stemCompletion(mat,corp.stemComp, type = c("longest"))
-    sc.df<-as.data.frame(sc)
-    #sc1.df<-as.data.frame(sc1)
-    #sc.df2<-as.data.frame(as.list(sc))
-    #fix(sc.df2)
-    ##Saving stem completed top topic top words in all documents
-    dflist<-list()
-    csum<-cumsum(n.obs)
-    for(i in 1:dim(corp.tdm.sp.t.tdif)[1]){
-      if(i==1){dflist[[i]]<-sc.df[1:n.obs[i],]
-      dflist[[i]]<-as.character(dflist[[i]])
-      }else{dflist[[i]]<-sc.df[(csum[i-1]+1):csum[i],]
-      dflist[[i]]<-as.character(dflist[[i]])
-      }
-    }
-
-    n.obs2 <- sapply(dflist, length)
-    seq.max2 <- seq_len(max(n.obs2))
-    mat22 <- t(sapply(dflist, "[", i = seq.max))
-    mat22<-t(mat22)
-
-    testmatr<-as.data.frame(mat22)
-    for(i in 1:dim(corp.tdm.sp.t.tdif)[1]){
-      testmatr[,i] <- sub("^$", "None", testmatr[,i])
-    }
-
-    mat<-testmatr
-  }
-
-
-  mat.t<-rbind(mat,t)
-  #fix(mat.t)
 
   tdftdf<-as.data.frame(ds[-zeros])
-  tmatdf<-as.data.frame(t(mat.t))
 
 
 
-  ##
-  mat2<-mat
-  mat.t2<-mat2
-  tempmatt<-mat.t2
-  mat.t2<-as.data.frame(mat.t2)
-  #mat.t<-mat.t[,-1]
-  mat.ttt<-t(mat.t2)
-  mat.ttt<-as.data.frame(mat.ttt)
-  #fix(mat.tt)
-  #mat.tt$x <- paste(mat.tt$`1`,mat.tt$`2`,mat.tt$V3,mat.tt$V4,mat.tt$V5,mat.tt$V6)
-  mat.ttt$x <- paste(mat.ttt[,1], ";",mat.ttt[,2],";",mat.ttt[,3],";",mat.ttt[,4],";",mat.ttt[,5],";",mat.ttt[,6])
-  mat.ttt$x<-gsub("NA","",mat.ttt$x)
-  tmp<-cbind(mat.ttt$x,t)
-  tmp<-as.data.frame(tmp)
+  t<-t[-zeros]
+  terms.df<-as.data.frame(terms(myModel,20000000000))
 
-  ##
-
-  results.list<-list(tmp,zeros,terms.df)
+  results.list<-list(t,zeros,terms.df)
 
   return(results.list)
 
